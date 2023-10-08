52643da7dbd9df7c81879c751c83fdf8d1e83be8
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-10-08 11:03:03 +0300

    Add Categories Section

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,91 @@
+import 'package:fitness/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    _getCategories();
     return Scaffold(
         appBar: appBar(),
         backgroundColor: Colors.white,
         body: Column(
-          children: [_searchField()],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _searchField(),
+            SizedBox(
+              height: 40,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    'Category',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 120,
+                  child: ListView.separated(
+                    itemCount: categories.length,
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    separatorBuilder: (context, index) => SizedBox(
+                      width: 25,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: screenWidth / categories.length,
+                        decoration: BoxDecoration(
+                            color: categories[index].boxColor.withOpacity(0.3),
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    shape: BoxShape.circle),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SvgPicture.asset(
+                                      categories[index].iconPath),
+                                ),
+                              ),
+                              Text(
+                                categories[index].name,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black,
+                                    fontSize: 14),
+                              )
+                            ]),
+                      );
+                    },
+                  ),
+                )
+              ],
+            )
+          ],
         ));
   }
 
