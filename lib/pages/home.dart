e40e1ab1e0d91191c802451697b9667c01955f19
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-10-08 10:36:27 +0300

    Add AppBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: const Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)),
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/icons/Arrow - Left 2.svg',
+              height: 20,
+              width: 20,
+            ),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: const Color(0xffF7F8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              alignment: Alignment.center,
+              width: 37,
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                height: 5,
+                width: 5,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
