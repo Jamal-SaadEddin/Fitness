b7b171d8bc150bff79012d0a01280c5e59e43a51
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-10-08 10:44:46 +0300

    Add Search Field

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
-    );
+        appBar: appBar(),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Color(0xff1D1617).withOpacity(0.11),
+                    blurRadius: 40,
+                    spreadRadius: 0.0)
+              ]),
+              child: TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: EdgeInsets.all(15),
+                    hintText: 'Search Pancake',
+                    hintStyle:
+                        TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: SvgPicture.asset('assets/icons/Search.svg'),
+                    ),
+                    suffixIcon: Container(
+                      width: 100,
+                      child: IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            VerticalDivider(
+                              color: Colors.black,
+                              indent: 10,
+                              endIndent: 10,
+                              thickness: 0.1,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child:
+                                  SvgPicture.asset('assets/icons/Filter.svg'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide.none)),
+              ),
+            )
+          ],
+        ));
   }
 
   AppBar appBar() {
